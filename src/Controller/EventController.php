c8ca125c470555ff8f77c2dd6f8e989d1806488c
Damien <damien@cefimmac08.local>
2018-03-12 15:48:01 +0100

    Event entity

---
+++ src/Controller/EventController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 12/03/2018
+ * Time: 15:39
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Entity\User;
+use App\Entity\Event;
+
+/**
+ * Class EventController
+ * @package App\Controller
+ */
+class EventController extends Controller
+{
+    /**
+     * Page d'affichage d'un évènement
+     * @Route ("/event/{id}", name="page_evenement", requirements={"id": "\d+"})
+     * @param Event $event
+     * @return Response
+     */
+    public function userPage(Event $event)
+    {
+        // TODO : affichage d'un profil
+        return $this->render("event.html.twig",
+            array("event" => $event));
+    }
+}
---
+++ src/Entity/Event.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EventRepository")
+ */
+class Event
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=1000)
+     */
+    private $place;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=3000)
+     */
+    private $description;
+
+    /// CONSTRUCTEUR
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /// GETTEURS / SETTEURS
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+
+    /**
+     * @param mixed $place
+     */
+    public function setPlace($place): void
+    {
+        $this->place = $place;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date): void
+    {
+        $this->date = new \DateTime($date);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+
+}
