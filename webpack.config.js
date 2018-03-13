8347e7be03bbe7ba9b32d02ca1d8972c5fd7a641
Damien <damien@cefimmac08.local>
2018-03-13 17:24:24 +0100

    bootstrap integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
+// the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
@@ -19,6 +19,13 @@ Encore
 
     // uncomment for legacy applications that require $/jQuery as a global variable
     // .autoProvidejQuery()
+
+    .enableSassLoader(function (sassOptions) {
+    }, {
+        resolveUrlLoader: false
+    })
+    .autoProvidejQuery()
+
 ;
 
 module.exports = Encore.getWebpackConfig();
